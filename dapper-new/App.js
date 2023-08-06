98514a5fccd15eafe5a0cdeb09b2a8bb2931999e
moumitabaral <moumitabaral005@gmail.com>
2023-08-06 14:58:44 +0600

    barber booking

--- dapper-new/App.js
+++ dapper-new/App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import BarberBooking from "./screens/customer/BarberBooking";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "poppins-regular": require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+    "poppins-medium": require("./assets/fonts/Poppins/Poppins-Medium.ttf"),
+    "poppins-semibold": require("./assets/fonts/Poppins/Poppins-SemiBold.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarberBooking />
     </View>
   );
 }
@@ -13,8 +22,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
