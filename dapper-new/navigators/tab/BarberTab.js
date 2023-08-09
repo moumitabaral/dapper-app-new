8be4f76cfedff7f8cf11ec55d784fcc014e86e4e
RaktimBanerje <braktim99@gmail.com>
2023-08-09 07:25:11 +0530

    Update

--- dapper-new/navigators/tab/BarberTab.js
+++ dapper-new/navigators/tab/BarberTab.js
@@ -11,74 +11,70 @@ const BarbarTab = () => {
     const Tab = createBottomTabNavigator();
 
     return (
-        null
+        <Tab.Navigator
+            screenOptions={{
+                tabBarActiveTintColor: "#AE8447",
+                tabBarInactiveTintColor: "black",
+                headerShown: false,
+                tabBarHideOnKeyboard: true
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={BarbarMainStack}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home-outline" size={20} color={color} />
+                    ),
+                }}
+            />
+
+            <Tab.Screen
+                name="Booking"
+                component={BarbarBookingStack}
+                options={{
+                    tabBarLabel: 'Booking',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="calendar-clock-outline" size={20} color={color} />
+                    ),
+                }}
+            />
+
+            <Tab.Screen
+                name="Notification"
+                component={BarbarNotificationStack}
+                options={{
+                    tabBarLabel: 'Notification',
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="bell" size={20} color={color} />
+                    ),
+                }}
+            />
+
+            <Tab.Screen
+                name="Inbox"
+                component={BarbarChatStack}
+                options={{
+                    tabBarLabel: 'Inbox',
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="message1" size={20} color={color} />
+                    ),
+                }}
+            />
+
+            <Tab.Screen
+                name="Profile"
+                component={BarbarProfileStack}
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="account-circle-outline" size={24} color={color} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
     )
-
-    // return (
-    //     <Tab.Navigator
-    //         screenOptions={{
-    //             tabBarActiveTintColor: "#AE8447",
-    //             tabBarInactiveTintColor: "black",
-    //             headerShown: false,
-    //             tabBarHideOnKeyboard: true
-    //         }}
-    //     >
-    //         <Tab.Screen
-    //             name="Home"
-    //             component={BarbarMainStack}
-    //             options={{
-    //                 tabBarLabel: 'Home',
-    //                 tabBarIcon: ({ color, size }) => (
-    //                     <Ionicons name="home-outline" size={20} color={color} />
-    //                 ),
-    //             }}
-    //         />
-
-    //         <Tab.Screen
-    //             name="Booking"
-    //             component={BarbarBookingStack}
-    //             options={{
-    //                 tabBarLabel: 'Booking',
-    //                 tabBarIcon: ({ color, size }) => (
-    //                     <MaterialCommunityIcons name="calendar-clock-outline" size={20} color={color} />
-    //                 ),
-    //             }}
-    //         />
-
-    //         <Tab.Screen
-    //             name="Notification"
-    //             component={BarbarNotificationStack}
-    //             options={{
-    //                 tabBarLabel: 'Notification',
-    //                 tabBarIcon: ({ color, size }) => (
-    //                     <FontAwesome5 name="bell" size={20} color={color} />
-    //                 ),
-    //             }}
-    //         />
-
-    //         <Tab.Screen
-    //             name="Inbox"
-    //             component={BarbarChatStack}
-    //             options={{
-    //                 tabBarLabel: 'Inbox',
-    //                 tabBarIcon: ({ color, size }) => (
-    //                     <AntDesign name="message1" size={20} color={color} />
-    //                 ),
-    //             }}
-    //         />
-
-    //         <Tab.Screen
-    //             name="Profile"
-    //             component={BarbarProfileStack}
-    //             options={{
-    //                 tabBarLabel: 'Profile',
-    //                 tabBarIcon: ({ color, size }) => (
-    //                     <MaterialCommunityIcons name="account-circle-outline" size={24} color={color} />
-    //                 ),
-    //             }}
-    //         />
-    //     </Tab.Navigator>
-    // )
 }
 
 export default BarbarTab
